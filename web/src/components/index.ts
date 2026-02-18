4eff1d425af9fbc7ed6726cfee21fd39363ce714
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-17 21:24:43 -0300

    feat: :sparkles: implement grid toggle functionality in Canvas and Toolbar components

---
+++ web/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Canvas } from "./Canvas.astro";
+export { default as Controls } from "./Controls.astro";
+export { default as Docs } from "./Docs.astro";
+export { default as Header } from "./Header.astro";
+export { default as Preview } from "./Preview.astro";
+export { default as Toolbar } from "./Toolbar.astro";
